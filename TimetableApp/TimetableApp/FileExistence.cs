d7a87e4d6416c08fb98061e5d6bff198a86418b8
Oleksandr Markov <ol.markov1999@gmail.com>
2021-03-08 22:36:04 +0200

    DbConnection, FileExistenceCheck

---
+++ TimetableApp/TimetableApp/FileExistence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; 
+
+namespace TimetableApp
+{
+    /*
+     Проверка существования Excel-файла
+     в директории
+     */
+    class FileExistence
+    {
+        string fileName;
+        const string dirName = @"E:\BACHELORS WORK\TIMETABLE\data";
+
+        public FileExistence(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public bool Exists()
+        {
+            string full_path_to_file = String.Concat(dirName, @"\", fileName);
+
+            if (Directory.Exists(dirName))
+            {
+                string[] files = Directory.GetFiles(dirName);
+
+                if (files.Contains(full_path_to_file))
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Файл не обнаружен: " +  "\"" + full_path_to_file + "\"");
+                    return false;
+                }              
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Пути \"" + dirName + "\" не существует!");
+                return false;
+            }
+        }
+    }
+}
+
---
+++ TimetableApp/TimetableApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DBConnectionTest.Test();
+
+            FileExistence file = new FileExistence("Auditory_types.xlsx");
+
+            if (file.Exists())
+            {
+                //проверка содержимого файла (дубликаты, пустые строки, некорректные значения и т.д.)
+
+                // что делать если бд пуста/заполнена/связные таблицы ...
+
+                // загрузка
+
+            }
+
+            //Console.WriteLine(String.Concat("1", "2343", "asda"));
+
+            Console.ReadKey();
+        }
+    }
+}
