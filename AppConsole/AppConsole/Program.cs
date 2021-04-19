2019e6ced9e52149f19409317b1ca0cfb8596d5f
Oleksandr Markov <ol.markov1999@gmail.com>
2021-04-19 05:47:30 +0300

    Dep_ComputerSystemsAndNetworks class

--- AppConsole/AppConsole/Program.cs
+++ AppConsole/AppConsole/Program.cs
@@ -19,20 +19,20 @@ namespace AppConsole
 			
 
 			
-			//Dep_MachineParts machineParts = new Dep_MachineParts("VIDOMOST_DORUChEN_2 сем_ДВ_ДМ і ПТМ.xlsx", 15, 50);
+			Dep_MachineParts machineParts = new Dep_MachineParts("VIDOMOST_DORUChEN_2 сем_ДВ_ДМ і ПТМ.xlsx", 15, 50);
 			
-			//Dep_MachineBuildingTechnology mbt = new Dep_MachineBuildingTechnology("ВІДОМІСТЬ ДОРУЧЕНЬ ТМБ денне весна - 2020.xlsx", 15, 46);
+			Dep_MachineBuildingTechnology mbt = new Dep_MachineBuildingTechnology("ВІДОМІСТЬ ДОРУЧЕНЬ ТМБ денне весна - 2020.xlsx", 15, 46);
 			
-			//Dep_EconomyAndCustoms economyAndCustoms = new Dep_EconomyAndCustoms("Економіки та митної справи_Форма 44 ВІДОМІСТЬ ДОРУЧЕНЬ - 2020_ЕМС.xlsx", 15, 84);
+			Dep_EconomyAndCustoms economyAndCustoms = new Dep_EconomyAndCustoms("Економіки та митної справи_Форма 44 ВІДОМІСТЬ ДОРУЧЕНЬ - 2020_ЕМС.xlsx", 15, 84);
 			
-			//Dep_EconomicalTheory economicalTheory = new Dep_EconomicalTheory("ЕКОНОМІЧНОЇ ТЕОРІЇ ТА ПІДПРИЄМНИЦТВА_ВІДОМІСТЬ ДОРУЧЕНЬ - 2020.xlsx", 15, 79);
+			Dep_EconomicalTheory economicalTheory = new Dep_EconomicalTheory("ЕКОНОМІЧНОЇ ТЕОРІЇ ТА ПІДПРИЄМНИЦТВА_ВІДОМІСТЬ ДОРУЧЕНЬ - 2020.xlsx", 15, 79);
 			
-			//Dep_ElectricalMachines electricalMachines = new Dep_ElectricalMachines("Електричних_машин-Форма 44 ВІД ДОРУЧЕНЬ- 2020_кафЕМ_ден2 сем.xlsx", 15, 45);
+			Dep_ElectricalMachines electricalMachines = new Dep_ElectricalMachines("Електричних_машин-Форма 44 ВІД ДОРУЧЕНЬ- 2020_кафЕМ_ден2 сем.xlsx", 15, 45);
 			
-			//Dep_IndustrialEnergySupply industrialEnergySupply = new Dep_IndustrialEnergySupply("Електропостачання промислових підприємств_Форма 44 ЕПП - 2020д.xlsx", 15, 76);
+			Dep_IndustrialEnergySupply industrialEnergySupply = new Dep_IndustrialEnergySupply("Електропостачання промислових підприємств_Форма 44 ЕПП - 2020д.xlsx", 15, 76);
 			
-			//Dep_ComputerSystemsAndNetworks computerSystemsAndNetworks_sheet1 = new Dep_ComputerSystemsAndNetworks("КОМП_ЮТЕРНІ СИСТЕМИ ТА МЕРЕЖІ_ВІДОМІСТЬ ДОРУЧЕНЬ_19_20.xlsx", 15, 93, 1);
-			//Dep_ComputerSystemsAndNetworks computerSystemsAndNetworks_sheet2 = new Dep_ComputerSystemsAndNetworks("КОМП_ЮТЕРНІ СИСТЕМИ ТА МЕРЕЖІ_ВІДОМІСТЬ ДОРУЧЕНЬ_19_20.xlsx", 15, 23, 2);
+			Dep_ComputerSystemsAndNetworks computerSystemsAndNetworks_sheet1 = new Dep_ComputerSystemsAndNetworks("КОМП_ЮТЕРНІ СИСТЕМИ ТА МЕРЕЖІ_ВІДОМІСТЬ ДОРУЧЕНЬ_19_20.xlsx", 15, 93, 1);
+			Dep_ComputerSystemsAndNetworks computerSystemsAndNetworks_sheet2 = new Dep_ComputerSystemsAndNetworks("КОМП_ЮТЕРНІ СИСТЕМИ ТА МЕРЕЖІ_ВІДОМІСТЬ ДОРУЧЕНЬ_19_20.xlsx", 15, 23, 2);
 			
 			Dep_MarketingAndLogistics marketingAndLogistics = new Dep_MarketingAndLogistics("МАРКЕТИНГУ ТА ЛОГІСТИКИ_Відомість_денне_ІІ_нова.xls", 15, 72);
 			
@@ -46,17 +46,16 @@ namespace AppConsole
 			
 			//відомості
 			
-			//machineParts.SendDataToDB();
-			//mbt.SendDataToDB();
-			//economyAndCustoms.SendDataToDB();
-			//economicalTheory.SendDataToDB();
-			//electricalMachines.SendDataToDB();
-			//industrialEnergySupply.SendDataToDB();
+			machineParts.SendDataToDB();
+			mbt.SendDataToDB();
+			economyAndCustoms.SendDataToDB();
+			economicalTheory.SendDataToDB();
+			electricalMachines.SendDataToDB();
+			industrialEnergySupply.SendDataToDB();
 			
-			
-			//computerSystemsAndNetworks_sheet1.SendDataToDB();
-			//computerSystemsAndNetworks_sheet2.SendDataToDB();
-			//Console.WriteLine("ComputerSystemsAndNetworks Department is loaded!");
+			computerSystemsAndNetworks_sheet1.SendDataToDB();
+			computerSystemsAndNetworks_sheet2.SendDataToDB();
+			Console.WriteLine("ComputerSystemsAndNetworks Department is loaded!");
 			
 			marketingAndLogistics.SendDataToDB();
 			
