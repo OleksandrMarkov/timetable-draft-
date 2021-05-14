ff1a7a64d561bc0de843ac50bdb74d95fdc16b84
Oleksandr Markov <ol.markov1999@gmail.com>
2021-05-14 20:23:44 +0300

    fixed loading way for AM departmnent

---
+++ DataCollectionApp/DataCollectionApp/DbOperations.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using MySql.Data.MySqlClient;
+
+namespace DataCollectionApp
+{
+	public class DbOperations
+	{	
+		MySqlConnection mySqlConnection = DBUtils.GetDBConnection();
+		MySqlCommand mySqlCommand;
+		MySqlDataReader mySqlDataReader;
+		
+		public ArrayList getDepartments()
+		{
+			ArrayList departments = new ArrayList();		
+			mySqlConnection.Open();
+			const string getDepartments = "SELECT full_name FROM department WHERE department_id NOT BETWEEN 60 AND 66";
+			mySqlCommand = new MySqlCommand (getDepartments, mySqlConnection);
+			using(MySqlDataReader dataReader = mySqlCommand.ExecuteReader())
+			{
+				while(dataReader.Read())
+				{
+					departments.Add(dataReader[0].ToString());
+				}
+			}
+			mySqlConnection.Close();		
+			return departments;
+		}
+		
+		public int getDepartmentID(string departmentName)
+		{
+			const string command = "SELECT department_id FROM department WHERE short_name = @DEPARTMENT";	
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@DEPARTMENT", departmentName);
+			mySqlCommand.ExecuteNonQuery();
+			int departmentID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
+			mySqlConnection.Close();
+			return departmentID;
+		}
+		
+		public int getDisciplineID(string discipline)
+		{
+			const string command = "SELECT discipline_id FROM discipline WHERE full_name = @DISCIPLINE";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@DISCIPLINE", discipline);
+			mySqlCommand.ExecuteNonQuery();
+			int disciplineID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
+			mySqlConnection.Close();
+			return disciplineID;
+		}
+		
+		public void insertLesson(int disciplineID, string lessonsType, int hours, string lessonsControl, int departmentID)
+		{
+			const string command = "INSERT INTO lesson (discipline_id, type, countOfHours, control, department_id) "
+					+ "VALUES (@DISCIPLINE_ID, @TYPE, @HOURS, @CONTROL, @DEPARTMENT_ID)";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@DISCIPLINE_ID", disciplineID);
+			mySqlCommand.Parameters.AddWithValue("@TYPE", lessonsType);
+			mySqlCommand.Parameters.AddWithValue("@HOURS", hours);
+			mySqlCommand.Parameters.AddWithValue("@CONTROL", lessonsControl);
+			mySqlCommand.Parameters.AddWithValue("@DEPARTMENT_ID", departmentID);
+			mySqlCommand.ExecuteNonQuery();
+			mySqlConnection.Close();
+		}
+		
+		public int getLastLessonID()
+		{
+			const string command = "SELECT lesson_id FROM lesson ORDER BY lesson_id DESC LIMIT 1";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.ExecuteNonQuery();
+			int lessonID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());			
+			mySqlConnection.Close();
+			return lessonID;
+		}
+		
+		public int getAuditoryID(string auditory)
+		{
+			const string command = "SELECT auditory_id FROM auditory WHERE auditory_name = @AUDITORY";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@AUDITORY", auditory);
+			mySqlCommand.ExecuteNonQuery();
+			int auditoryID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
+			mySqlConnection.Close();
+			return auditoryID;
+		}
+		
+		public void insertLesson_Auditory(int lastLessonID, int auditoryID)
+		{
+			const string command = "INSERT INTO lesson_auditory (lesson_id, auditory_id) "
+					+ "VALUES (@LESSON_ID, @AUDITORY_ID)";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@LESSON_ID", lastLessonID);
+			mySqlCommand.Parameters.AddWithValue("@AUDITORY_ID", auditoryID);
+			mySqlCommand.ExecuteNonQuery();
+			mySqlConnection.Close();
+		}
+		
+		public int getTeacherID(string teacher)
+		{
+			const string command = "SELECT teacher_id FROM teacher WHERE full_name = @TEACHER";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@TEACHER", teacher);
+			mySqlCommand.ExecuteNonQuery();
+			int teacherID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
+			mySqlConnection.Close();
+			return teacherID;
+		}
+		
+		public void insertLesson_Teacher(int lastLessonID, int teacherID)
+		{
+			const string command = "INSERT INTO lesson_teacher (lesson_id, teacher_id) "
+					+ "VALUES (@LESSON_ID, @TEACHER_ID)";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@LESSON_ID", lastLessonID);
+			mySqlCommand.Parameters.AddWithValue("@TEACHER_ID", teacherID);
+			mySqlCommand.ExecuteNonQuery();
+			mySqlConnection.Close();
+		}
+		
+		public ArrayList getStudy_groups()
+		{
+			const string command = "SELECT full_name FROM study_group";
+			ArrayList studyGroups = new ArrayList();
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlDataReader = mySqlCommand.ExecuteReader();
+			while(mySqlDataReader.Read())
+			{
+				studyGroups.Add(mySqlDataReader[0].ToString());
+			}
+			mySqlConnection.Close();
+			return studyGroups;
+		}
+		
+		public void insertStudy_group(int departmentID, string name, string code)
+		{
+			const string command = "INSERT INTO study_group (department_id, full_name, study_group_code) "
+			+ "VALUES (@DEPARTMENT_ID, @NAME, @CODE)";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@DEPARTMENT_ID", departmentID);
+			mySqlCommand.Parameters.AddWithValue("@NAME", name);
+			mySqlCommand.Parameters.AddWithValue("@CODE", code);
+			mySqlCommand.ExecuteNonQuery();
+			mySqlConnection.Close();
+		}
+		
+		public int getStudy_groupID(string study_group)
+		{
+			const string command = "SELECT study_group_id FROM study_group WHERE full_name = @GROUP";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@GROUP", study_group);
+			mySqlCommand.ExecuteNonQuery();
+			int study_groupID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
+			mySqlConnection.Close();
+			return study_groupID;	
+		}
+		
+		public void insertLesson_group(int lessonID, int groupID)
+		{
+			const string command = "INSERT INTO lesson_group (lesson_id, group_id) "
+			+ "VALUES (@LESSON_ID, @GROUP_ID)";
+			mySqlConnection.Open();
+			mySqlCommand = new MySqlCommand(command, mySqlConnection);
+			mySqlCommand.Parameters.AddWithValue("@LESSON_ID", lessonID);
+			mySqlCommand.Parameters.AddWithValue("@GROUP_ID", groupID);
+			mySqlCommand.ExecuteNonQuery();
+			mySqlConnection.Close();
+		}
+	}
+}
--- DataCollectionApp/DataCollectionApp/Dep_AppliedMathematics.cs
+++ DataCollectionApp/DataCollectionApp/Dep_AppliedMathematics.cs
@@ -28,20 +28,25 @@ namespace DataCollectionApp
 		const char teachersColumn = 'H';
 		const char auditoriesColumn = 'I';
 		
+		int sheetNumber;
+		string departmentShortName;
+		
 		bool reading = true;
 				
-		public Dep_AppliedMathematics(string fileName, int firstRow, int lastRow): base(fileName)
+		public Dep_AppliedMathematics(string fileName, int firstRow, int lastRow, int sheetNumber, string departmentShortName): base(fileName)
 		{
 			this.fileName = fileName;
 			this.firstRow = firstRow;
 			this.lastRow = lastRow;
+			this.sheetNumber = sheetNumber;
+			this.departmentShortName = departmentShortName;
 		}
 		
 		public override void ReadFromExcelFile()
 		{
 			try
 			{
-				open(1);
+				open(sheetNumber);
 				for(int col = getColumnNumber(disciplinesColumn), i = firstRow; i <= lastRow; i++)
 				{
 					cellContent = getCellContent(i, col);
@@ -95,146 +100,74 @@ namespace DataCollectionApp
 			{
 				try
 				{
-					MySqlConnection connection = DBUtils.GetDBConnection();
-					MySqlCommand mySqlCommand;
+					DbOperations dbo = new DbOperations();
+					CellContentCorrection ccc = new CellContentCorrection();
 					
-					const string selectDepartmentID = "SELECT department_id FROM department WHERE short_name = @DEPARTMENT";
-					const string selectDisciplineID = "SELECT discipline_id FROM discipline WHERE full_name = @DISCIPLINE";					
-					const string insertLessons = "INSERT INTO lesson (discipline_id, type, countOfHours, control, department_id) "
-					+ "VALUES (@DISCIPLINE_ID, @TYPE, @HOURS, @CONTROL, @DEPARTMENT_ID)";
-					const string selectLessonID = "SELECT lesson_id FROM lesson ORDER BY lesson_id DESC LIMIT 1";
-					const string selectTeacherID = "SELECT teacher_id FROM teacher WHERE full_name = @TEACHER";
-					const string selectAuditoryID = "SELECT auditory_id FROM auditory WHERE auditory_name = @AUDITORY";
-					const string insertLesson_teacher = "INSERT INTO lesson_teacher (lesson_id, teacher_id) "
-					+ "VALUES (@LESSON_ID, @TEACHER_ID)";
-					const string insertLesson_auditory = "INSERT INTO lesson_auditory (lesson_id, auditory_id) "
-					+ "VALUES (@LESSON_ID, @AUDITORY_ID)";
 					
-					connection.Open();
-					
-					mySqlCommand = new MySqlCommand(selectDepartmentID, connection);
-					mySqlCommand.Parameters.AddWithValue("@DEPARTMENT", "ЕМ");
-					mySqlCommand.ExecuteNonQuery();
-						
-					int departmentID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
+					int departmentID = dbo.getDepartmentID("ПМ");
 					
 					for(int i = 0; i < disciplines.Count; i++)
 					{
-						mySqlCommand = new MySqlCommand(selectDisciplineID, connection);
-						mySqlCommand.Parameters.AddWithValue("@DISCIPLINE", disciplines[i]);
-						mySqlCommand.ExecuteNonQuery();
+						int disciplineID = dbo.getDisciplineID(disciplines[i].ToString());
 						
-						int disciplineID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
-		
-						mySqlCommand = new MySqlCommand(insertLessons, connection);
-						mySqlCommand.Parameters.AddWithValue("@DISCIPLINE_ID", disciplineID);
-						mySqlCommand.Parameters.AddWithValue("@TYPE", lessonsType[i]);
-						mySqlCommand.Parameters.AddWithValue("@HOURS", hours[i]);
-						mySqlCommand.Parameters.AddWithValue("@CONTROL", lessonsControl[i]);
-						mySqlCommand.Parameters.AddWithValue("@DEPARTMENT_ID", departmentID);
-						mySqlCommand.ExecuteNonQuery();
+						dbo.insertLesson(disciplineID, lessonsType[i].ToString(), Convert.ToInt32(hours[i]),
+						lessonsControl[i].ToString(), departmentID);
 						
-						mySqlCommand = new MySqlCommand(selectLessonID, connection);
-						mySqlCommand.ExecuteNonQuery();
-						
-						int lessonID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());					
+						int lastLessonID = dbo.getLastLessonID();
+												
 						string suggestedAuditories = auditories[i].ToString();
 						
 						if(suggestedAuditories.Length != 0)
-						{
-							suggestedAuditories = suggestedAuditories.TrimEnd(new char [] {',', ';'});
-							suggestedAuditories = suggestedAuditories.Replace(" ", "");
-						
+						{					
+							suggestedAuditories = ccc.correctAuditories(suggestedAuditories);
+							
 							string [] separatedAuditories = suggestedAuditories.Split(new char[] {',', ';'});
+							
 							for (int j = 0; j < separatedAuditories.Length; j++)
 							{
-								mySqlCommand = new MySqlCommand(selectAuditoryID, connection);
-								mySqlCommand.Parameters.AddWithValue("@AUDITORY", separatedAuditories[j]);
-								mySqlCommand.ExecuteNonQuery();
-						
-								int auditoryID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
-										
-								mySqlCommand = new MySqlCommand(insertLesson_auditory, connection);
-								mySqlCommand.Parameters.AddWithValue("@LESSON_ID", lessonID);
-								mySqlCommand.Parameters.AddWithValue("@AUDITORY_ID", auditoryID);
-								mySqlCommand.ExecuteNonQuery();
+								int auditoryID = dbo.getAuditoryID(separatedAuditories[j]);
+								dbo.insertLesson_Auditory(lastLessonID, auditoryID);
 							}	
 						}
 
 						string teachersRecord = teachers[i].ToString();
-						teachersRecord = teachersRecord.TrimEnd(new char [] {',', ';'});
+						teachersRecord = ccc.correctTeachers(teachersRecord);
+						
 						string [] separatedTeachers = teachersRecord.Split(new char[] {',', ';'});
 			
 						for(int j = 0; j < separatedTeachers.Length; j++)
 						{
 							separatedTeachers[j] = separatedTeachers[j].Trim();
-							mySqlCommand = new MySqlCommand(selectTeacherID, connection);
-							mySqlCommand.Parameters.AddWithValue("@TEACHER", separatedTeachers[j]);
-							mySqlCommand.ExecuteNonQuery();
 							
-							int teacherID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());							
+							int teacherID = dbo.getTeacherID(separatedTeachers[j]);
 							
-							mySqlCommand = new MySqlCommand(insertLesson_teacher, connection);
-							mySqlCommand.Parameters.AddWithValue("@LESSON_ID", lessonID);
-							mySqlCommand.Parameters.AddWithValue("@TEACHER_ID", teacherID);
-							mySqlCommand.ExecuteNonQuery();
+							dbo.insertLesson_Teacher(lastLessonID, teacherID);
 						}
-						
-						const string selectStudyGroups = "SELECT full_name FROM study_group";
-						const string insertLesson_group = "INSERT INTO lesson_group (lesson_id, group_id) "
-						+ "VALUES (@LESSON_ID, @GROUP_ID)";
-						const string insertStudy_group = "INSERT INTO study_group (department_id, full_name, study_group_code) "
-						+ "VALUES (@DEPARTMENT_ID, @NAME, @CODE)";					
-						const string selectGroupID = "SELECT study_group_id FROM study_group WHERE full_name = @GROUP";
 						
 						string groupsInCell = groups[i].ToString();
 						if(groupsInCell.Length != 0)
 						{
-							groupsInCell = groupsInCell.Trim();
-							groupsInCell = groupsInCell.TrimEnd(new char [] {',', ';'});
-							groupsInCell = groupsInCell.Replace(" ", "");
-							groupsInCell = groupsInCell.Replace("\n", "");					
-							string [] separatedGroups = groupsInCell.Split(new char[] {',', ';'});
-						
-							ArrayList studyGroupsFromDB = new ArrayList();
-							mySqlCommand = new MySqlCommand(selectStudyGroups, connection);
-							using (MySqlConnection connection2 = DBUtils.GetDBConnection())
-							{
-								using(MySqlDataReader dataReader = mySqlCommand.ExecuteReader())
-								{
-									while(dataReader.Read())
-									{
-										studyGroupsFromDB.Add(dataReader[0].ToString());
-									}
-								}
-							}
+							groupsInCell = ccc.correctGroups(groupsInCell);
 							
+							string [] separatedGroups = groupsInCell.Split(new char[] {',', ';'});
+							
+							ArrayList studyGroups = dbo.getStudy_groups();
+								
 							for(int j = 0; j < separatedGroups.Length; j++)
 							{
-								if (studyGroupsFromDB.Contains(separatedGroups[j]) == false)
+								if (studyGroups.Contains(separatedGroups[j]) == false)
 								{
-									int hyphen = separatedGroups[j].IndexOf('-');
-									string code = separatedGroups[j].Substring(0, hyphen + 2);
+									GroupCode gc = new GroupCode();
+									string code = gc.getGroupCode(separatedGroups[j]);
 									
-									mySqlCommand = new MySqlCommand(insertStudy_group, connection);
-									mySqlCommand.Parameters.AddWithValue("@DEPARTMENT_ID", departmentID);
-									mySqlCommand.Parameters.AddWithValue("@NAME", separatedGroups[j]);
-									mySqlCommand.Parameters.AddWithValue("@CODE", code);
-									mySqlCommand.ExecuteNonQuery();
+									dbo.insertStudy_group(departmentID, separatedGroups[j], code);
 								}
-								mySqlCommand = new MySqlCommand(selectGroupID, connection);
-								mySqlCommand.Parameters.AddWithValue("@GROUP", separatedGroups[j]);
-								mySqlCommand.ExecuteNonQuery();
-								int groupID = Convert.ToInt32(mySqlCommand.ExecuteScalar().ToString());
 								
-								mySqlCommand = new MySqlCommand(insertLesson_group, connection);
-								mySqlCommand.Parameters.AddWithValue("@LESSON_ID", lessonID);
-								mySqlCommand.Parameters.AddWithValue("@GROUP_ID", groupID);
-								mySqlCommand.ExecuteNonQuery();
+								int groupID = dbo.getStudy_groupID(separatedGroups[j]);
+								dbo.insertLesson_group(lastLessonID, groupID);
 							}
 						}
 					}		
-					connection.Close();
 				}
 				catch(Exception ex)
 				{
